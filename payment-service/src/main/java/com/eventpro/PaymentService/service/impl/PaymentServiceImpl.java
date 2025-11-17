322bb46d545b61f73e289e42d4e799613a5b718e
RobsonPierno <robson.pierno@gmail.com>
2025-11-17 10:53:57 +0100

    CircuitBreak implementation

--- payment-service/src/main/java/com/eventpro/PaymentService/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/eventpro/PaymentService/service/impl/PaymentServiceImpl.java
@@ -11,6 +11,8 @@ import com.eventpr.SalesService.dto.SaleDTO;
 import com.eventpro.PaymentService.producer.KafkaProducer;
 import com.eventpro.PaymentService.service.PaymentService;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 @Service
 public class PaymentServiceImpl implements PaymentService {
 
@@ -20,14 +22,29 @@ public class PaymentServiceImpl implements PaymentService {
 	private KafkaProducer kafkaProducer;
 	
 	@Override
+	@CircuitBreaker(name = "paymentService", fallbackMethod = "fallbackCall")
 	public void paymentProcess(final SaleDTO saleDto) {
-		log.debug("paymentProcess({})", saleDto);
+		log.info("paymentProcess({})", saleDto);
 		
 		if (new Random().nextBoolean()) {
-			log.info("PAYMENT COMPLETED WITH SUCCESS! -> {}", saleDto);
+			log.info("PAYMENT SERVICE 1: SUCCESS! -> {}", saleDto);
 			this.kafkaProducer.paymentSuccess(saleDto);
 		} else {
-			log.warn("PAYMENT FAILED! -> {}", saleDto);
+			log.warn("PAYMENT SERVICE 1: FAILED! -> {}", saleDto);
+			throw new RuntimeException();
+			
+		}
+	}
+	
+	@SuppressWarnings("unused")
+	private void fallbackCall(final SaleDTO saleDto, Throwable ex) {
+		log.info("fallbackCall({},{})", saleDto, ex);
+		
+		if (new Random().nextBoolean()) {
+			log.info("PAYMENT SERVICE 2: SUCCESS! -> {}", saleDto);
+			this.kafkaProducer.paymentSuccess(saleDto);
+		} else {
+			log.warn("PAYMENT SERVICE 2: FAILED! -> {}", saleDto);
 			this.kafkaProducer.paymentFail(saleDto);
 		}
 	}
